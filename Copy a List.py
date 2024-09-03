78c98727f834b3104d5f5caaedfeee7534cf56b4
Samsul Arafin Shanto <154091776+arsep95f@users.noreply.github.com>
2024-09-04 02:36:49 +0600

    Add files via upload

---
+++ Copy a List.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy() #copy#
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist) #list() method#
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:] #slice Operator#
+print(mylist)
