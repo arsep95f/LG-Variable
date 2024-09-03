3c061d127a7a42f38ad9ae07c5b535831aa9f07a
Samsul Arafin Shanto <154091776+arsep95f@users.noreply.github.com>
2024-09-04 02:41:42 +0600

    Add files via upload

---
+++ Join Lists.py
@@ -0,0 +1,20 @@
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x) #Append list2 into list1#
+
+print(list1)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
